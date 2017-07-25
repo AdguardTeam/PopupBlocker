366200d2b446c508d29768bc84619a9889e8ac25
seanl-adg <seanl@adguard.com>
2017-07-25 09:19:22 +0900

    Update meta.js

--- meta.js
+++ meta.js
@@ -1,8 +1,34 @@
 // ==UserScript==
-// @name PopupBlocker
+// @name Adguard Popup Blocker
+// @name:ru	Блокировщик всплывающей рекламы Adguard
+// @name:uk Блокувальник спливаючої реклами Adguard
+// @name:de	Adguard Popup-Blocker
+// @name:sr	Koristi Adguard-ov blokator iskačućih prozora
+// @name:pl	Bloker wyskakujących okienek
+// @name:zh	使用 Adguard 弹窗拦截器
+// @name:zh-TW	Adguard 彈出視窗阻擋器
+// @name:sk	Adguard blokovač vyskakovacích okien
+// @name:fr	Bloqueur de popup de Adguard
+// @name:it	Blocco Pop-Up di Adguard
+// @name:es	Bloqueador Popup de Adguard
 // @namespace Adguard
-// @description Blocks popups preemptively by wrapping browser APIs for additional checks
+// @description	Blocks popup ads on web pages
+// @description:ru	Блокирует всплывающую рекламу на страницах
+// @description:uk  Блокує спливаючу рекламу на веб-сторінках
+// @description:de	Blockiert Anzeige-Popups auf Webseiten
+// @description:tr	Web sayfalarında açılan pencere reklamları engeller
+// @description:ko	웹 페이지의 팝업 광고를 차단 합니다.
+// @description:sr	Blokira iskačuće reklame na veb stranicama
+// @description:pl	Blokuje wyskakujące okienka z reklamami na stronach internetowych
+// @description:zh	拦截网页弹窗广告
+// @description:zh-TW	阻擋網頁彈窗廣告
+// @description:sk	Blokuje vyskakovacie reklamy na webových stránkach
+// @description:fr	Bloque les publicités intrusives sur les pages web
+// @description:it	Blocca gli annunci di popup nelle pagine internet
+// @description:vi	Chặn quảng cáo popup trên các trang web
+// @description:es	Bloquea popups de anuncios en sitios web
 // @version 2.0
+// @adg:permanent	true
 // @downloadURL [DOWNLOAD_URL]
 // @updateURL [UPDATE_URL]
 // @match http://*/*
