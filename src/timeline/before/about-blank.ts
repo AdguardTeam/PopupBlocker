a8d3e57f6d48ce4f9ed5afec0904f4d91c4f6c11
seanl-adg <seanl@adguard.com>
2017-08-08 17:46:02 +0900

    Fix

--- src/timeline/before/about-blank.ts
+++ src/timeline/before/about-blank.ts
@@ -2,7 +2,6 @@ import { condition } from '../index';
 import { TimelineEvent, TLEventType } from '../event';
 import getTime from '../time';
 import * as log from '../../log';
-import { isMocked } from '../../dom/utils/mock-window';
 
 const aboutBlank:condition = (index, events) => {
     // if there is a blocked popup within 100 ms, do not allow opening popup with url about:blank.
