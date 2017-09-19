d6db6c37e39d0fa557db5722cccbfa1b96119f69
seanl-adg <seanl@adguard.com>
2017-09-19 17:47:19 +0900

    Increment version

--- meta.js
+++ meta.js
@@ -27,7 +27,7 @@
 // @description:it	Blocca gli annunci di popup nelle pagine internet
 // @description:vi	Chặn quảng cáo popup trên các trang web
 // @description:es	Bloquea popups de anuncios en sitios web
-// @version 2.1.5
+// @version 2.1.6
 // @downloadURL [DOWNLOAD_URL]
 // @updateURL [UPDATE_URL]
 // @supportURL https://github.com/AdguardTeam/PopupBlocker/issues
