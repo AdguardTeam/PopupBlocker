AdguardTeam/PopupBlocker

commit 0871c9e0c502c849eadf65424ddd747ee612bd52

Add location check for timeline's "create" test

src/timeline/before/create.ts
@@ -1,5 +1,6 @@
 import { condition } from '../index';
 import { TimelineEvent, TLEventType } from '../event';
+import { ABOUT_PROTOCOL } from '../../shared/dom';
 import getTime from '../../shared/time';
 import * as log from '../../shared/log';
 
@@ -7,7 +8,9 @@ const createOpen:condition = (index, events) => {
     log.print('index:', index);
     let evt = events[index][0];
     if (evt.$type == TLEventType.CREATE && getTime() - evt.$timeStamp < 200) {
-        return false;
+        if (window.location.protocol === ABOUT_PROTOCOL) {
+            return false;
+        }
     }
     return true;
 };

gulpfile.js
@@ -140,6 +140,20 @@ gulp.task('release', (done) => {
     runSequence('clean', 'prep-tscc', 'tsickle', 'meta', 'tscc', 'tscc-clean', done);
 });
 
+
+
+gulp.task('dev-minified', (done) => {
+    options.channel = "Dev";
+    options.cc_options = cc_opt.ts;
+    options.preprocessContext = {
+        NO_PROXY: true,
+        DEBUG: true,
+        RECORD: true
+    };
+    runSequence('clean', 'prep-tscc', 'tsickle', 'meta', 'tscc', 'tscc-clean', done);
+})
+
+
 gulp.task('release-no-minification', (done) => {
     options.channel = "Release-No-Min";
     options.rollup_options = rollup_opt.dev;

src/shared/dom.ts
@@ -17,7 +17,8 @@ export const getTagName = (el:Node):string => {
 /**
  * Detects about:blank, about:srcdoc urls.
  */
-const reEmptyUrl = /^about\:/;
+export const ABOUT_PROTOCOL = 'about:';
+const reEmptyUrl = new RegExp('^' + ABOUT_PROTOCOL);
 export const isEmptyUrl = (url:string):boolean => {
     return reEmptyUrl.test(url);
 };

commit eaa105a431657ba8891f26422b875f3a17db1b6e

Fix regression caused by a commit 0871c9e0c502c849eadf65424ddd747ee612bd52 https://github.com/AdguardTeam/PopupBlocker/commit/d0efead535538c1fc3a520bcdaa50cf0f7b0290d

src/platform/extension/edge/content_script.ts
@@ -1,3 +1,4 @@
+import chrome from '../shared/platform_namespace';
 import AlertController from '../../../ui/alert_controller';
 import ExtensionStorageManager from '../shared/storage/ExtensionStorageManager';
 import EdgeI18nService from './localization/EdgeI18nService'

src/timeline/before/create.ts
@@ -18,7 +18,7 @@ const createOpen:condition = (index, events) => {
          * calls window.open with empty url in order to obtain reference to certain browsing
          * context.
          */
-        if (window.location.protocol === ABOUT_PROTOCOL) {
+        if ((<Window>evt.$data).location.protocol === ABOUT_PROTOCOL) {
             return false;
         }
     }

src/timeline/index.ts
@@ -61,10 +61,11 @@ export default class Timeline {
         log.callEnd();
         return true;
     }
-    onNewFrame():number {
+    onNewFrame(window:Window):number {
         let pos = this.events.push([]) - 1;
         // Registers a unique event when a frame is first created.
-        this.registerEvent(new TimelineEvent(TLEventType.CREATE, undefined, undefined), pos);
+        // It passes the `window` object of the frame as a value of `$data` property.
+        this.registerEvent(new TimelineEvent(TLEventType.CREATE, undefined, window), pos);
         return pos;
     }
     /**
@@ -101,7 +102,7 @@ export default class Timeline {
 }
 
 export const timeline:Timeline = typeof PARENT_FRAME_KEY === 'string' ? window.parent[PARENT_FRAME_KEY][2] : new Timeline();
-export const position:number = typeof PARENT_FRAME_KEY === 'string' ? timeline.onNewFrame() : 0;
+export const position:number = typeof PARENT_FRAME_KEY === 'string' ? timeline.onNewFrame(window) : 0;
 
 // These are called from the outside of the code, so we have to make sure that call structures of those are not modified.
 // It is removed in minified builds, see the gulpfile.