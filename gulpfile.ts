AdguardTeam/PopupBlocker

commit 0871c9e0c502c849eadf65424ddd747ee612bd52

Add location check for timeline's "create" test

src/timeline/before/create.ts
@@ -1,5 +1,6 @@
 import { condition } from '../index';
 import { TimelineEvent, TLEventType } from '../event';
+import { ABOUT_PROTOCOL } from '../../shared/dom';
 import getTime from '../../shared/time';
 import * as log from '../../shared/log';
 
@@ -7,7 +8,9 @@ const createOpen:condition = (index, events) => {
     log.print('index:', index);
     let evt = events[index][0];
     if (evt.$type == TLEventType.CREATE && getTime() - evt.$timeStamp < 200) {
-        return false;
+        if (window.location.protocol === ABOUT_PROTOCOL) {
+            return false;
+        }
     }
     return true;
 };

gulpfile.js
@@ -140,6 +140,20 @@ gulp.task('release', (done) => {
     runSequence('clean', 'prep-tscc', 'tsickle', 'meta', 'tscc', 'tscc-clean', done);
 });
 
+
+
+gulp.task('dev-minified', (done) => {
+    options.channel = "Dev";
+    options.cc_options = cc_opt.ts;
+    options.preprocessContext = {
+        NO_PROXY: true,
+        DEBUG: true,
+        RECORD: true
+    };
+    runSequence('clean', 'prep-tscc', 'tsickle', 'meta', 'tscc', 'tscc-clean', done);
+})
+
+
 gulp.task('release-no-minification', (done) => {
     options.channel = "Release-No-Min";
     options.rollup_options = rollup_opt.dev;

src/shared/dom.ts
@@ -17,7 +17,8 @@ export const getTagName = (el:Node):string => {
 /**
  * Detects about:blank, about:srcdoc urls.
  */
-const reEmptyUrl = /^about\:/;
+export const ABOUT_PROTOCOL = 'about:';
+const reEmptyUrl = new RegExp('^' + ABOUT_PROTOCOL);
 export const isEmptyUrl = (url:string):boolean => {
     return reEmptyUrl.test(url);
 };

commit 89ec554dd49abba864ca28f5478f9d3edbfcde60

Merge pull request #19 in EXTENSIONS/popup-blocker from extension-migration-fix to master
Squashed commit of the following:

commit eaa105a431657ba8891f26422b875f3a17db1b6e
Author: seanl-adg <seanl@adguard.com>
Date:   Fri Jan 19 16:13:10 2018 +0900

    Fix regression caused by a commit 0871c9e0c502c849eadf65424ddd747ee612bd52
    https://github.com/AdguardTeam/PopupBlocker/commit/d0efead535538c1fc3a520bcdaa50cf0f7b0290d

commit 1892236ecb75e0b71a7a9387f6bbb6315fdaa088
Author: seanl-adg <seanl@adguard.com>
Date:   Fri Jan 19 15:20:35 2018 +0900

    Fix for Edge

gulpfile.ts
@@ -119,7 +119,7 @@ class PathUtils {
         [BuildTarget.USERSCRIPT]:   'platform/userscript/content_script.ts',
         [BuildTarget.CHROME]:       'platform/extension/shared/content_script.ts',
         [BuildTarget.FIREFOX]:      'platform/extension/shared/content_script.ts',
-        [BuildTarget.EDGE]:         'platform/extension/shared/content_script.ts'
+        [BuildTarget.EDGE]:         'platform/extension/edge/content_script.ts'
     }
     public get contentScriptEntry() {
         return path.posix.join(
@@ -384,6 +384,7 @@ export default class Builder {
         const sorter = new ManifestSort(manifest);
         const deps = sorter.getDeps([this.paths.commonEntryCc, this.paths.pageScriptEntryCc, this.paths.contentScriptEntryCc]);
         const flags = [
+            '--charset',                  'UTF-8',
             '--compilation_level',        'ADVANCED',
             '--language_in',              'ECMASCRIPT6',
             '--language_out',             'ECMASCRIPT5',

src/localization/I18nService.ts
@@ -17,7 +17,7 @@ function toHtmlSafeString(str:string):string {
 
 export default class I18nService implements II18nService {
     constructor(
-        public getMessage:(messageId:string)=>string
+        public $getMessage:(messageId:string)=>string
     ) { }
 
     /**
@@ -84,7 +84,7 @@ export default class I18nService implements II18nService {
             val = current.nodeValue;
             if (I18nService.reCommentPh.test(val)) {
                 val = val.slice(5);
-                let message = this.getMessage(val);
+                let message = this.$getMessage(val);
                 let parsed = this.parseMessage(message, context);
                 let pr:Element = <Element><any>current.parentNode;
                 tasks.push(new InsertTask(pr, current,

src/localization/II18nService.ts
@@ -2,7 +2,7 @@ export default interface II18nService {
     /**
      * Analogous to chrome.i18n.getMessage.
      */
-    getMessage(nessageId:string):string
+    $getMessage(nessageId:string):string
     /**
      * Replace placeholders contained in a string `message` (usually obtained via getMessage
      * call) with respect to a `context` dictionary.

src/platform/extension/edge/content_script.ts
@@ -1,13 +1,14 @@
+import chrome from '../shared/platform_namespace';
 import AlertController from '../../../ui/alert_controller';
-import ExtensionStorageManager from './storage/ExtensionStorageManager';
-import I18nService from '../../../localization/I18nService';
+import ExtensionStorageManager from '../shared/storage/ExtensionStorageManager';
+import EdgeI18nService from './localization/EdgeI18nService'
 import * as log from '../../../shared/log';
-import linkPageScript from './storage/link_page_script';
+import linkPageScript from '../shared/storage/link_page_script';
 
 /**************************************************************************/
 /**************************************************************************/
 
-const i18nService       = new I18nService(chrome.i18n.getMessage);
+const i18nService       = new EdgeI18nService(chrome.i18n.getMessage);
 const storageManager    = new ExtensionStorageManager();
 const alertController   = new AlertController(i18nService, storageManager);
 

src/platform/extension/edge/localization/EdgeI18nService.ts
@@ -1,0 +1,16 @@
+import I18nService from "../../../../localization/I18nService";
+
+/**
+ * Edge extension api does not replace `$$` to `$` in internationalized messages.
+ * Since there is no way to obtain a single `$` from the api, we have to replace it
+ * from an overridden `getMessage` method.
+ */
+export default class EdgeI18nService extends I18nService {
+    private static reDoubleDollarMark:RegExp = /\$\$/g;
+    constructor(getMessage:(messageId:string)=>string) {
+        super(getMessage);
+        this.$getMessage = (messageId:string):string => {
+            return getMessage(messageId).replace(EdgeI18nService.reDoubleDollarMark, '$');
+        }
+    }
+}

src/platform/extension/shared/storage/link_page_script.ts
@@ -1,0 +1,70 @@
+import chrome from '../platform_namespace';
+import { CONTENT_PAGE_MAGIC, DownwardMsgTypesEnum, UpwardMsgTypesEnum, UpwardMsgTypes, SettingsDeltaMsg, Settings } from "../MessageTypes";
+import AlertController from "../../../../ui/alert_controller";
+import * as log from '../../../../shared/log';
+
+export default function (alertController:AlertController) {
+
+    const receivePort = new Promise<MessagePort>(function(resolve, reject) {
+        window.addEventListener("message", function(event) {
+            if (event.source !== window) { return; }
+            if (event.data !== CONTENT_PAGE_MAGIC) { return; }
+            log.print(`contentscript: received a port...`);
+            const port:MessagePort = event.ports[0];
+            event.stopImmediatePropagation();
+            resolve(port);
+        });
+    });
+
+    function onMessage(message:MessageEvent) {
+        const data:UpwardMsgTypes = message.data;
+        switch (data.$type) {
+            case UpwardMsgTypesEnum.SETTINGS_CHANGE:
+                chrome.storage.local.set(message.data);
+                break;
+            case UpwardMsgTypesEnum.CREATE_ALERT:
+                alertController.createAlert(data.orig_domain, data.popup_url);
+        }
+    }
+
+    receivePort
+        .then(function(port) {
+            port.onmessage = onMessage;
+        });
+
+    const getSettings = new Promise(function(resolve, reject) {
+        chrome.storage.local.get(["whitelist", location.host], function(items) {
+            resolve(items);
+        });
+    });
+
+    Promise.all([receivePort, getSettings])
+        .then(function(resolved) {
+            const port = resolved[0];
+            const settings = resolved[1];
+            port.postMessage(<SettingsDeltaMsg>{
+                $type: DownwardMsgTypesEnum.SETTINGS_DELTA,
+                settings: settings
+            });
+        });
+
+    chrome.storage.onChanged.addListener(function(changes, namespace) {
+        if (namespace !== 'local') { return; }
+        let partialSettings:Partial<Settings> = {};
+        let storageChange;
+        if ('whitelisted' in changes) {
+            storageChange = changes['whitelisted'];
+            partialSettings.whitelistedDestinations = storageChange.newVal;
+        }
+        if (location.hostname in changes) {
+            storageChange = changes[location.hostname];
+            partialSettings.originIsWhitelisted = storageChange.newValue;
+        }
+        receivePort.then(function(port) {
+            port.postMessage(<SettingsDeltaMsg>{
+                $type: DownwardMsgTypesEnum.SETTINGS_DELTA,
+                settings: partialSettings
+            });
+        });
+    });
+}

src/timeline/before/create.ts
@@ -18,7 +18,7 @@ const createOpen:condition = (index, events) => {
          * calls window.open with empty url in order to obtain reference to certain browsing
          * context.
          */
-        if (window.location.protocol === ABOUT_PROTOCOL) {
+        if ((<Window>evt.$data).location.protocol === ABOUT_PROTOCOL) {
             return false;
         }
     }

src/timeline/index.ts
@@ -61,10 +61,11 @@ export default class Timeline {
         log.callEnd();
         return true;
     }
-    onNewFrame():number {
+    onNewFrame(window:Window):number {
         let pos = this.events.push([]) - 1;
         // Registers a unique event when a frame is first created.
-        this.registerEvent(new TimelineEvent(TLEventType.CREATE, undefined, undefined), pos);
+        // It passes the `window` object of the frame as a value of `$data` property.
+        this.registerEvent(new TimelineEvent(TLEventType.CREATE, undefined, window), pos);
         return pos;
     }
     /**
@@ -101,7 +102,7 @@ export default class Timeline {
 }
 
 export const timeline:Timeline = typeof PARENT_FRAME_KEY === 'string' ? window.parent[PARENT_FRAME_KEY][2] : new Timeline();
-export const position:number = typeof PARENT_FRAME_KEY === 'string' ? timeline.onNewFrame() : 0;
+export const position:number = typeof PARENT_FRAME_KEY === 'string' ? timeline.onNewFrame(window) : 0;
 
 // These are called from the outside of the code, so we have to make sure that call structures of those are not modified.
 // It is removed in minified builds, see the gulpfile.

src/ui/alert_controller.ts
@@ -119,7 +119,7 @@ class Alert implements AlertIntf {
     }
     // Until we come up with a proper UI, we reponse to user interaction with window.confirm.
     private showConfirmationDialog (messageId:string, context:StringMap):boolean {
-        const message = this.i18nService.formatText(this.i18nService.getMessage(messageId), context);
+        const message = this.i18nService.formatText(this.i18nService.$getMessage(messageId), context);
         // Certain browsers always return `false` from window.confirm;
         // In such cases, we skip confirmation step.
         // https://github.com/AdguardTeam/PopupBlocker/issues/50