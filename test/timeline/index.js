1e063b6ae63136fa3457c55ff3425ef4b19311ec
seanl-adg <seanl@adguard.com>
2018-04-15 21:09:47 +0900

    Add title and footer to userscript settings page

--- test/timeline/index.js
+++
@@ -1,9 +0,0 @@
-import { timeline } from '../../src/timeline/index';
-const expect = chai.expect;
-describe('Timeline', function () {
-    it('should log when it is first created', function () {
-        let records = timeline.takeRecords();
-        let firstEvent = timeline.takeRecords()[0][0];
-        expect(firstEvent.$type).to.equal(0 /* CREATE */);
-    });
-});
