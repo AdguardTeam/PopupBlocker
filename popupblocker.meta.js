8f69875c8554ba6c30ef7ec22448f329774a1c98
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-18 07:30:31 +0000

    Deploy AdguardTeam/PopupBlocker to github.com/AdguardTeam/PopupBlocker.git:gh-pages

--- popupblocker.meta.js
+++ popupblocker.meta.js
@@ -1,34 +1,48 @@
 // ==UserScript==
 // @name AdGuard Popup Blocker (Dev)
-// @name:fr Bloqueur de fenêtres pop-up de AdGuard (Dev)
+// @name:ar AdGuard Popup Blocker (Dev)
+// @name:cs Blok. vyskak. oken AdGuard (Dev)
+// @name:da AdGuard Popup Blocker (Dev)
 // @name:de AdGuard Pop-up-Blocker (Dev)
 // @name:es Bloqueador de popup de AdGuard (Dev)
-// @name:ar AdGuard Popup Blocker (Dev)
+// @name:fa مسدودساز پاپ-آپ AdGuard (Dev)
+// @name:fr Bloqueur de fenêtres pop-up de AdGuard (Dev)
+// @name:id AdGuard Popup Blocker (Dev)
 // @name:it Blocco Pop-Up di AdGuard (Dev)
-// @name:ru Блокировщик всплывающей рекламы от AdGuard (Dev)
-// @name:pt-BR AdGuard Bloqueador de Pop-ups (Dev)
-// @name:zh-TW AdGuard 彈出式視窗封鎖器 (Dev)
-// @name:da AdGuard Popup Blocker (Dev)
-// @name:tr AdGuard Popup Blocker (Dev)
 // @name:ja AdGuard ポップアップブロッカー (Dev)
-// @name:zh-CN AdGuard 弹窗拦截器 (Dev)
+// @name:no AdGuards popup-blokkerer (Dev)
 // @name:pl Bloker wyskakujących okienek przez AdGuard (Dev)
+// @name:pt-BR AdGuard Bloqueador de Pop-ups (Dev)
+// @name:pt-PT AdGuard Popup Blocker (Dev)
+// @name:ru Блокировщик всплывающей рекламы от AdGuard (Dev)
+// @name:sl-SI AdGuard Blokirnik pojavnih oken (Dev)
+// @name:tr AdGuard Popup Blocker (Dev)
+// @name:uk Блокувальник спливаючої реклами AdGuard (Dev)
+// @name:zh-CN AdGuard 弹窗拦截器 (Dev)
+// @name:zh-TW AdGuard 彈出式視窗封鎖器 (Dev)
 // @namespace AdGuard
 // @description Blocks popup ads on web pages
-// @description:fr Bloque les fenêtres pop-up avec publicités intrusives sur les pages web
+// @description:ar لحظر الإعلانات المنبثقة على صفحات الويب
+// @description:cs Blokuje vyskakovací reklamy na webových stránkách
+// @description:da Blokerer pop-up reklamer på websider
 // @description:de Blockiert Anzeige-Pop-ups auf Webseiten
 // @description:es Bloquea elementos emergentes en sitios web
-// @description:ar لحظر الإعلانات المنبثقة على صفحات الويب
+// @description:fa مسدودسازی تبلیغات پاپ آپ در صفحات وب.
+// @description:fr Bloque les fenêtres pop-up avec publicités intrusives sur les pages web
+// @description:id Blokir iklan popup di halaman web
 // @description:it Blocca gli annunci di popup nelle pagine internet
-// @description:ru Блокирует всплывающую рекламу на страницах
-// @description:pt-BR Bloqueia anúncios pop-ups dentro dos sites
-// @description:zh-TW 封鎖於網頁上之彈出式視窗廣告
-// @description:da Blokerer pop-up reklamer på websider
-// @description:tr Web sayfalarında açılan pencere reklamları engeller
 // @description:ja Webページでポップアップ広告をブロックします。
-// @description:zh-CN 拦截网页弹窗广告
+// @description:no Blokker popup-annonser på nettsider
 // @description:pl Blokuje wyskakujące okienka z reklamami na stronach internetowych
-// @version 2.5.9
+// @description:pt-BR Bloqueia anúncios pop-ups dentro dos sites
+// @description:pt-PT Bloqueia anúncios popup em páginas da web.
+// @description:ru Блокирует всплывающую рекламу на страницах
+// @description:sl-SI Blokira pojavne oglase na spletnih straneh
+// @description:tr Web sayfalarında açılan pencere reklamları engeller
+// @description:uk Блокує спливаючу рекламу на веб-сторінках
+// @description:zh-CN 拦截网页弹窗广告
+// @description:zh-TW 封鎖於網頁上之彈出式視窗廣告
+// @version 2.5.10
 // @license LGPL-3.0; https://github.com/AdguardTeam/PopupBlocker/blob/master/LICENSE
 // @downloadURL https://popupblocker.adguard.com/popupblocker.user.js
 // @updateURL https://popupblocker.adguard.com/popupblocker.meta.js
