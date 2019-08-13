2bcd3a805d2ae3656670dd6fc0eef25685ad9fef
Tim Shenin <t.shenin@adguard.com>
2019-08-13 17:25:39 +0300

    Merge pull request #65 in EXTENSIONS/popup-blocker from feature/twosky to master
    Squashed commit of the following:
    
    commit 9bb8dcaf358ac0397df01e032770846240934985
    Author: Tim Shenin <t.shenin@adguard.com>
    Date:   Tue Aug 13 14:19:28 2019 +0300
    
        Import crowdin files from twosky config
    
    commit 3686eab9d9af7d8f1d792906fb80d513a8d03413
    Author: Tim Shenin <t.shenin@adguard.com>
    Date:   Tue Aug 13 13:40:09 2019 +0300
    
        Add localizable path to twosky config
    
    commit 8979f0a6520fd50ac94a735746a3c7a0aed9a729
    Author: Tim Shenin <t.shenin@adguard.com>
    Date:   Tue Aug 13 11:58:20 2019 +0300
    
        Add twosky config
    
    commit 1c8bb2d6443508360bfbfeb150e7ef9235f48334
    Author: Tim Shenin <t.shenin@adguard.com>
    Date:   Fri Aug 9 14:33:05 2019 +0300
    
        Update translations
    
    commit 390e9c041bf96a47792da2e4a5bc518eed53dff2
    Author: Tim Shenin <t.shenin@adguard.com>
    Date:   Thu Aug 8 12:34:12 2019 +0300
    
        add desc
    
    commit a1f884887aa72c45005d51c740ea1952b24d2ea2
    Author: Tim Shenin <t.shenin@adguard.com>
    Date:   Thu Aug 8 12:16:26 2019 +0300
    
        Update version
    
    commit 74d920a949af2a89bc8d0f423ff7b654f01c2d4f
    Author: Tim Shenin <t.shenin@adguard.com>
    Date:   Thu Aug 8 12:00:43 2019 +0300
    
        Update locales
    
    commit b48041fe7bf25459ed9533e5164f8d45f6cc3c1c
    Author: Tim Shenin <t.shenin@adguard.com>
    Date:   Thu Aug 8 11:55:40 2019 +0300
    
        Add ability to add locales equivalents
    
    commit afd14e1e4fef24279e330bb073650234537be943
    Author: Tim Shenin <t.shenin@adguard.com>
    Date:   Wed Aug 7 20:10:47 2019 +0300
    
        Update trans
    
    commit 2f30252d0dff2329cff041cfc663cb6729d8b406
    Author: Tim Shenin <t.shenin@adguard.com>
    Date:   Tue Aug 6 16:11:48 2019 +0300
    
        Update message
    
    commit 4a6f7ccd3b6ca616f068e21514aa9cb74e7dbd13
    Author: Tim Shenin <t.shenin@adguard.com>
    Date:   Tue Aug 6 14:11:28 2019 +0300
    
        Update readme
    
    commit c5c00fd45130ffc314828a4d16d4c0efc8ee0cf2
    Author: Tim Shenin <t.shenin@adguard.com>
    Date:   Tue Aug 6 14:06:31 2019 +0300
    
        Remove old tasks
    
    commit 3224173e9c1d65640d96b9a86248b07fc1f83706
    Author: Tim Shenin <t.shenin@adguard.com>
    Date:   Tue Aug 6 13:55:37 2019 +0300
    
        Fix some phrases
    
    commit e43046787c2b342cb6cdd1269f9016818e7829d5
    Author: Tim Shenin <t.shenin@adguard.com>
    Date:   Tue Aug 6 13:40:12 2019 +0300
    
        Update translations
    
    commit 49dfc488ce9fd690bf30bbf1cfa53f67d30fae7d
    Author: Tim Shenin <t.shenin@adguard.com>
    Date:   Tue Aug 6 12:20:25 2019 +0300
    
        Fix uploading and downloading
    
    commit 0d57751d6048f5aaf7934d13df4499928a74949c
    Author: Tim Shenin <t.shenin@adguard.com>
    Date:   Mon Aug 5 18:39:40 2019 +0300
    
        Downloading files
    
    commit a0319d5a657d98fe408085e7687643fa5e2e7965
    Author: Tim Shenin <t.shenin@adguard.com>
    Date:   Fri Aug 2 19:02:53 2019 +0300
    
        Add locels downloading

---
+++ locales.js
@@ -0,0 +1,164 @@
+/* eslint-disable no-console */
+const fs = require('fs-extra');
+const path = require('path');
+const axios = require('axios');
+const FormData = require('form-data');
+const twoskyConfig = require('./.twosky.json')[0];
+
+// URLs
+const BASE_URL = 'https://twosky.adtidy.org/api/v1';
+const BASE_DOWNLOAD_URL = `${BASE_URL}/download`;
+const BASE_UPLOAD_URL = `${BASE_URL}/upload`;
+// Directory where locales should be stored
+const LOCALES_DIR = './src/locales';
+// Base locale
+const BASE_LOCALE = twoskyConfig.base_locale;
+// Twosky project see mapping https://twosky.adtidy.org/api/v1/mapping
+const CROWDIN_PROJECT = twoskyConfig.project_id;
+// Available translations list
+const LOCALES = Object.keys(twoskyConfig.languages);
+// Crowdin files for downloading/uploading
+const CROWDIN_FILES = twoskyConfig.localizable_files
+    .map(pathToFile => pathToFile.split('/').pop());
+
+/**
+ * Users locale may be defined with only two chars (language code)
+ * Here we provide a map of equivalent translation for such locales
+ */
+const LOCALES_EQUIVALENTS_MAP = {
+    'pt-BR': 'pt',
+    'zh-CN': 'zh',
+};
+
+/**
+ * Build query string for downloading tranlations
+ * @param {string} lang locale code
+ * @param {string} file crowdin file name
+ */
+const getQueryString = (lang, file) => {
+    let res = '?format=json';
+    res += `&language=${lang}`;
+    res += `&project=${CROWDIN_PROJECT}`;
+    res += `&filename=${file}`;
+    return res;
+};
+
+/**
+ * Build form data for uploading tranlation
+ * @param {string} file crowdin file name
+ */
+const getFormData = (file) => {
+    const pathToBaseFile = path.resolve(LOCALES_DIR, file);
+    const body = new FormData();
+
+    body.append('format', 'json');
+    body.append('language', BASE_LOCALE);
+    body.append('project', CROWDIN_PROJECT);
+    body.append('filename', `${file}`);
+    body.append('file', fs.createReadStream(pathToBaseFile));
+    return body;
+};
+
+/**
+ * Returns link for downloading translations
+ * @param {string} lang locale code
+ * @param {string} file crowdin file name
+ */
+const getDownloadlURL = (lang, file) => BASE_DOWNLOAD_URL + getQueryString(lang, file);
+
+/**
+ * Replaces object to array with values by passed key in message object
+ * 
+ * WHY WE USE IT?
+ * Crowdin transforms output and replaces arrays to objects,
+ * these transformations can cause the errors in application.
+ * 
+ * @param {string} key member of message object
+ * @param {Object} data translation for specific locale
+ */
+const replaceObjectToArray = (key, data) => {
+    const result = {};
+    Object.entries(data).forEach(([message, value]) => {
+        if (typeof value[key] === 'object') {
+            result[message] = {
+                ...value,
+                [key]: Object.values(value[key])
+            }
+        } else {
+            result[message] = value;
+        }
+    })
+
+    return result;
+}
+
+/**
+ * Returns equivalent of specified locale code
+ * @param {string} locale locale
+ */
+const getEquivalent = (locale) => {
+    return LOCALES_EQUIVALENTS_MAP[locale] || locale;
+}
+
+/**
+ * Save file by path with passed content
+ * @param {string} filePath path to file
+ * @param {any} content
+ */
+function saveFile(filePath, content) {
+    fs.outputJson(filePath, content, { spaces: 4 });
+}
+
+/**
+ * Entry point for downloading translations
+ */
+async function download() {
+    const translations = {};
+
+    for (lang of LOCALES) {
+        for (file of CROWDIN_FILES) {
+            try {
+                const { data } = await axios.get(getDownloadlURL(lang, file));
+                const formatted = replaceObjectToArray('platform', data);
+                const resultLocale = getEquivalent(lang);
+                translations[resultLocale] = formatted;
+            } catch (e) {
+                console.log(getDownloadlURL(lang, file));
+                console.log(e);
+            }
+        }
+    }
+
+    const filePath = path.resolve(LOCALES_DIR, 'translations.json');
+    saveFile(filePath, translations);
+    console.log('File "translations.json" was successfully updated');
+}
+
+/**
+ * Entry point for uploading translations
+ */
+function upload() {
+    CROWDIN_FILES.forEach(async (filename) => {
+        try {
+            const formData = getFormData(filename);
+            await axios.post(BASE_UPLOAD_URL, formData, {
+                contentType: 'multipart/form-data',
+                headers: formData.getHeaders(),
+            });
+            console.log('Successfully uploaded');
+        } catch (e) {
+            console.log(e);
+        }
+    });
+}
+
+/**
+ * You need set environment variable LOCALES=DOWNLOAD|UPLOAD when run the script
+ */
+if (process.env.LOCALES === 'DOWNLOAD') {
+    download();
+} else if (process.env.LOCALES === 'UPLOAD') {
+    upload();
+} else {
+    console.log('Option DOWNLOAD/UPLOAD locales is not set');
+}
