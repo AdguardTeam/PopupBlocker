330a6d945db49b0e85c29fae3ba7d42a127d3f6c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-16 14:52:52 +0000

    Deploy AdguardTeam/PopupBlocker to github.com/AdguardTeam/PopupBlocker.git:gh-pages

--- popupblocker.meta.js
+++ popupblocker.meta.js
@@ -42,7 +42,7 @@
 // @description:uk Блокує спливаючу рекламу на веб-сторінках
 // @description:zh 拦截网页弹窗广告
 // @description:zh-TW 封鎖於網頁上之彈出式視窗廣告
-// @version 2.5.17
+// @version 2.5.18
 // @license LGPL-3.0; https://github.com/AdguardTeam/PopupBlocker/blob/master/LICENSE
 // @downloadURL https://AdguardTeam.github.io/PopupBlocker/popupblocker.user.js
 // @updateURL https://AdguardTeam.github.io/PopupBlocker/popupblocker.meta.js
