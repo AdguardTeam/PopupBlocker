c1150378b7e1e0da3e34a1b9e4382f662420cbe9
Oleg Labudko <o.labudko@adguard.com>
2021-03-15 13:29:51 +0300

    Pull request: AG-7097 Fix a couple of false-positives
    Merge in EXTENSIONS/popup-blocker from fix/AG-7097 to master
    Fixes #210
    Fixes #215
    Fixes #224
    
    Squashed commit of the following:
    
    commit 42686f68fd8b8b8a47fb74af850c0b358743db0f
    Author: tvinzz <tvinzz@tvinzzs-MacBook-Pro.local>
    Date:   Mon Mar 15 11:23:34 2021 +0300
    
        added chaturbate.com to exclusions
    
    commit a136d309fc59e9d27629bf802219500a96b896b9
    Author: tvinzz <tvinzz@tvinzzs-MacBook-Pro.local>
    Date:   Mon Mar 15 02:43:05 2021 +0300
    
        added ilfattoquotidiano.it, vanis.io, senpa.io, astrogo.astro.com.my to exclusions

--- compiler/MetadataUtils.ts
+++ compiler/MetadataUtils.ts
@@ -338,7 +338,15 @@ export default class MetadataUtils {
         '*://ubi.com/*',
         '*://*.ubi.com/*',
         // https://github.com/AdguardTeam/PopupBlocker/issues/222
-        '*://*.sistic.com.sg/*'
+        '*://*.sistic.com.sg/*',
+        // https://github.com/AdguardTeam/PopupBlocker/issues/224
+        '*://*.ilfattoquotidiano.it/*',
+        // https://github.com/AdguardTeam/PopupBlocker/issues/215
+        '*://*.vanis.io/*',
+        '*://*.senpa.io/*',
+        // https://github.com/AdguardTeam/PopupBlocker/issues/210
+        '*://*.astrogo.astro.com.my/*',
+        '*://*.chaturbate.com/*'
     ];
 
     private static extension_exclusions = {
@@ -669,7 +677,15 @@ export default class MetadataUtils {
             '*://ubi.com/*',
             '*://*.ubi.com/*',
             // https://github.com/AdguardTeam/PopupBlocker/issues/222
-            '*://*.sistic.com.sg/*'
+            '*://*.sistic.com.sg/*',
+            // https://github.com/AdguardTeam/PopupBlocker/issues/224
+            '*://*.ilfattoquotidiano.it/*',
+            // https://github.com/AdguardTeam/PopupBlocker/issues/215
+            '*://*.vanis.io/*',
+            '*://*.senpa.io/*',
+            // https://github.com/AdguardTeam/PopupBlocker/issues/210
+            '*://*.astrogo.astro.com.my/*',
+            '*://*.chaturbate.com/*'
         ]
     }
 
