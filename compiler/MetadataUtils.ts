2a956afe52978b5fc721d0c03e5588aad71fe974
tvinzz <o.labudko@adguard.com>
2021-05-11 13:45:13 +0200

    added domain to exclusions

--- compiler/MetadataUtils.ts
+++ compiler/MetadataUtils.ts
@@ -350,6 +350,9 @@ export default class MetadataUtils {
         // https://github.com/AdguardTeam/PopupBlocker/issues/233
         '*://play.pl/*',
         '*://*.play.pl/*',
+        // https://github.com/AdguardTeam/PopupBlocker/issues/218
+        '*://findicons.com/*',
+        '*://*.findicons.com/*',
         // https://github.com/AdguardTeam/PopupBlocker/issues/214
         '*://web.de/*',
         '*://*.web.de/*',
@@ -697,6 +700,9 @@ export default class MetadataUtils {
             // https://github.com/AdguardTeam/PopupBlocker/issues/233
             '*://play.pl/*',
             '*://*.play.pl/*',
+            // https://github.com/AdguardTeam/PopupBlocker/issues/218
+            '*://findicons.com/*',
+            '*://*.findicons.com/*',
             // https://github.com/AdguardTeam/PopupBlocker/issues/214
             '*://web.de/*',
             '*://*.web.de/*',
