d823133bfbc877fc7e6c35ffc6360a6c910a6968
Oleg Labudko <o.labudko@adguard.com>
2021-06-03 13:14:39 +0300

    Pull request: #238 online.bfgruppe.de is broken
    Merge in EXTENSIONS/popup-blocker from fix/238_online.bfgruppe.de to master
    
    Squashed commit of the following:
    
    commit 394b926a0d1ca96b44a4390bd47f6976e267d9aa
    Author: Alex302 <alex302@adguard.com>
    Date:   Wed Jun 2 19:11:50 2021 +0300
    
        online.bfgruppe.de #238

--- compiler/MetadataUtils.ts
+++ compiler/MetadataUtils.ts
@@ -363,7 +363,10 @@ export default class MetadataUtils {
         '*://*.gmx.net/*',
         // https://github.com/AdguardTeam/PopupBlocker/issues/234
         '*://clashofclans.com/*',
-        '*://*.clashofclans.com/*'
+        '*://*.clashofclans.com/*',
+        // https://github.com/AdguardTeam/PopupBlocker/issues/238
+        '*://online.bfgruppe.de/*',
+        '*://*.online.bfgruppe.de/*'
     ];
 
     private static extension_exclusions = {
@@ -719,7 +722,10 @@ export default class MetadataUtils {
             '*://*.gmx.net/*',
             // https://github.com/AdguardTeam/PopupBlocker/issues/234
             '*://clashofclans.com/*',
-            '*://*.clashofclans.com/*'
+            '*://*.clashofclans.com/*',
+            // https://github.com/AdguardTeam/PopupBlocker/issues/238
+            '*://online.bfgruppe.de/*',
+            '*://*.online.bfgruppe.de/*'
         ]
     }
 
